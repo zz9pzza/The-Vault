22c0bf6250a3897a7ed0e4a05bf2224535851262
James Beal <zz9pzza@gmail.com>
2012-04-08 20:28:35 +0000

    Catch up

---
+++ web/cgi/Vault/core.pm
@@ -0,0 +1,89 @@
+package Vault::core ;
+
+use VaultDB::Schema;
+use YAML qw(LoadFile);
+use MIME::Lite;
+use Template;
+use Template::Config;
+use Carp ;
+
+
+
+
+
+sub new  {
+    my($class, %args) = @_;
+
+    my $settings_file = exists $args{settings_file} ? $args{settings_file} : '/etc/vault' ;
+
+    my $self = {} ;
+    if ( ! -f $settings_file ) {
+		croak "Can't read $settings_file" ;
+    }
+
+    $self-> {'settings'}= LoadFile($settings_file);
+    $self-> {'config_file'} = $settings_file;
+    $self-> {'schema'} = VaultDB::Schema->connect('DBI:mysql:vault:'.$self-> {'host'}.':'.$self-> {'port'},$self-> {'settings'}->{database},$self-> {'settings'}->{password});
+
+
+    bless $self,$class;
+    return $self;
+  }
+
+sub get_default_value {
+  my($self,$request,$result) = @_ ;
+
+  my $new_result=$self-> {'settings'}->{$request} ;
+  if ( defined $new_result ) {
+	return $new_result ;
+  }
+  if ($result eq "NONE" ) {
+    return '';
+  }
+  if ( defined ( $result ) ) {
+	return $result ;
+  }
+  croak "Config option ($request) not set in config file (".$self-> {'config_file'}.')';
+}
+
+sub send_validation_email {
+  my($self,$email,$email_validation,$username) = @_ ;
+
+  my $tt = Template->new( EVAL_PERL => 1, INCLUDE_PATH => $self->get_default_value('email_template_dir'));
+  my $data ="" ;
+  my $vars   = {
+       url              => $self->get_default_value('cgi_path').'confirm_email' ,
+       username         => $username,
+	   email_validation => $email_validation
+     } ;
+
+  if (!$tt->process('email_registration.html', $vars,\$data) ) {
+    $data='<p>There has been an error: '.  $tt->error( ) . '<p>';
+  }
+
+  my $msg = MIME::Lite->new
+    (
+     Subject => $self->get_default_value('register_subject','Vault registration'),
+     From    => $self->get_default_value('register_from'),
+     To      => $email,
+     Cc      => $self->get_default_value('register_cc','NONE'),
+     Type    => 'multipart/related',
+  );
+
+  $msg->attach(
+    Type => 'text/html',
+    Data    => $data
+  );
+
+  $msg->attach(
+    Type => 'image/png',
+    Id   => 'logo.png',
+    Path => $self->get_default_value('email_template_image')
+  );
+
+ return $msg->send();
+
+}
+
+
+1;
